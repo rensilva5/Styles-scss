2fb2cd974e0f56f0962a4a37531f5a46f70c2fc2
rensilva5 <renzoasilva@gmail.com>
2022-08-21 23:35:33 -0400

    updates what it was seem in class

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import './styles/_master.scss'
+
+
+function App() {
+  return (
+    <div className='App'>
+    <h1>our app</h1>
+    <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <>
+        <ul className="navigation">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Links</a></li>
+        </ul>
+        <div className="menu">
+            <a href="" className="menu_link"></a>
+            <a href="" className="menu_link"></a>
+            <a href="" className="menu_link"></a>
+        </div>
+        </>
+
+    )
+}
+
+export default Header
